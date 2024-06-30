8caf9e1196fa5bd786cf715bdc82e5f9fca59cf1
Aditya-Garg10 <adityagarg646@gmail.com>
2024-06-30 10:53:31 +0530

    fourth commit

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,10 @@ app.use(cors())
 app.use(express.json())
 
 
+app.get("/",(req,res)=>{
+  res.json("Hello")
+})
+
 app.use('/auth', require('./routes/auth'))
 app.use('/notes', require('./routes/notes'))
 
